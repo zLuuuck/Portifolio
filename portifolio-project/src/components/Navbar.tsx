1e650b2352acb5423048342f4be6a272efe77d95
zLuuuck <xx.lyck.nitgh.xx@gmail.com>
2025-04-11 18:33:59 -0300

    teste

--- portifolio-project/src/components/Navbar.tsx
+++ portifolio-project/src/components/Navbar.tsx
@@ -7,6 +7,7 @@ function Navbar() {
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Lucas</span>
                     </a>
                     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                        <a href="https://linktr.ee/zluuuck"><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Me Contrate</button></a>
                         <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
